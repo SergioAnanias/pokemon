90410715aed990496f5103e6947b49f8021ad6e1
Sergio Ananias <sergio.ananias.o@gmail.com>
2021-05-04 11:06:23 -0400

    Pokemon primera generación de pokemons

--- script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    for(let i=1; i<=151; i++)
-    {
-        $('div').http('<img src=raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/', i, '.png>')
-    }
-});
